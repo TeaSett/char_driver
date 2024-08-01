2fc00ac4606ade761a7f34055a990b5e80403520
tea_sett <1136z56@gmail.com>
2024-08-01 16:12:17 +0300

    small fixes

--- print_operation_info.c
+++ print_operation_info.c
@@ -3,9 +3,9 @@
 #include "my_device.h"
 
 void print_operation_info(const char *op) {
-    struct 	timespec64 last_write_time;
-    ktime_get_ts64(&last_read_time);
+    struct 	timespec64 last_op_time;
+    ktime_get_ts64(&last_op_time);
     const struct cred *cred = current_cred();
 
-    printk(INFO_KERN DRIVER_NAME ": %s\nUID: %d; PID: %d; last_time: %d", op, cred->uid.val, current->pid, last_write_time);
+    printk(KERN_INFO DRIVER_NAME ": %s\nUID: %d; PID: %d; last_time: %d", op, cred->uid.val, current->pid, last_op_time);
 }
