bb83c64239f344f62f5035990527bc391dd1b31c
tea_sett <1136z56@gmail.com>
2024-08-04 17:36:30 +0300

    Blocking input

--- file_operations.c
+++ file_operations.c
@@ -21,10 +21,16 @@ int release_d(struct inode *inode, struct file *filp) {
 
 static DECLARE_WAIT_QUEUE_HEAD(wait_queue_for_readers);
 static DECLARE_WAIT_QUEUE_HEAD(wait_queue_for_writers);
+static bool someone_waits_for_output = false;
 
 //TODO: check 'len' value
 ssize_t read_from_d(struct file *filp, char __user *ubuf, size_t len, loff_t *offset) {
     print_operation_info("START READING");
+
+    if(my_device.write_blocking) {
+        someone_waits_for_output = true;
+        wake_up_interruptible(&wait_queue_for_writers);
+    }
 
     if(my_device.read_blocking) {
         wait_event_interruptible(wait_queue_for_readers, ((buffer.wend != buffer.start)||((buffer.wend == buffer.rstart)&&(buffer.wend != buffer.start))));
@@ -39,6 +45,8 @@ ssize_t read_from_d(struct file *filp, char __user *ubuf, size_t len, loff_t *of
     (*offset) += to_copy;
     wake_up_interruptible(&wait_queue_for_writers);
 
+    if(my_device.write_blocking) someone_waits_for_output = false;
+
     print_operation_info("STOP READING");
 
     return to_copy;
@@ -50,6 +58,8 @@ ssize_t write_to_d(struct file *filp, const char __user *ubuf, size_t len, loff_
 
     wait_event_interruptible(wait_queue_for_writers, (buffer.wend != buffer.end)||(buffer.rstart != buffer.start));
     if(buffer.rstart != buffer.start) buffer.wend = buffer.rstart = buffer.start;
+
+    if(my_device.write_blocking) wait_event_interruptible(wait_queue_for_writers, someone_waits_for_output);
 
     int to_copy = min(size, len);
 
